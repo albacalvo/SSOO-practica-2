af2521d1c99669e70a951625d7fab4ecb28043f2
albacalvo <albacalvoherrero1@gmail.com>
2021-01-12 19:26:44 +0100

    Minishell completa

--- archivos_c_h_apoyo/minishell.c
+++ archivos_c_h_apoyo/minishell.c
@@ -13,28 +13,39 @@
 #include "entrada_minishell.h"
 #include "ejecutar.h"
 
+static void manejar_sigchild(int signo){  //Cuando uno de los procesos finaliza, se elimina al zombie
+	int estado;
+	waitpid(-1,&estado,WNOHANG); //Espera a que finalice el proceso y guarda su estado en ese momento.Para conseguir que el proceso no se bloquee utilizamos WHOHANG
+}
 
 int main(int argc, char *argv[])
 {
 
-   char buf[BUFSIZ];
-   char *orden;
-   char *cadena;
+	char buf[BUFSIZ]; //almacenenamos la dirección del puntero en ese momento
+	char *orden;
+	char *cadena;
+	struct sigaction sa; //Para el manejo de señales creamos una estructura independiente
+
+	memset(&sa,0,sizeof(sa)); //Nos permite variar el comportamiento de la señal
+	sa.sa_handler = manejar_sigchild; //Recogemos los procesos que han muerto
+	sa.sa_flags = SA_NOCLDSTOP | SA_RESTART; //Con la primera flag hacemos que no se nos devuelva nada cuando el hijo zombie muere. La segunda flag controla que sucede cuando se ejecutan determinadas acciones(open, read, write) para que el manejador de la señal vuelva a su estado normal
+
+	sigaction(SIGCHLD, &sa, NULL);//Cuando recibe una determinada señal nos permite variar la accion que se lleva a cabo un proceso con esta funcion
 
 
    while (1)
    {
    
-	imprimir_prompt();
-	leer_linea_ordenes(buf);
-	cadena = strdup(buf);
-	while((orden = strsep(&cadena, ";")) !=0){	
-		if(strcmp(orden, "exit") !=0){
-		       if(es_ord_interna(orden)!=0){
-			       ejecutar_ord_interna(orden);
+	imprimir_prompt();	 //Imprime el prompt por pantalla
+	leer_linea_ordenes(buf);//Lee la orden introducida por le ususario
+	cadena = strdup(buf);	//Crea un duplicado de la orden introducida por el usuario
+	while((orden = strsep(&cadena, ";")) !=0){		//Extrae un elemento de la cadena
+		if(strcmp(orden, "exit") !=0){			//Si es 0 ejecuta el exit
+		       if(es_ord_interna(orden)!=0){		//Comprueba si es orden interna
+			       ejecutar_ord_interna(orden);	//Ejecuta la orden interna
 			}
 		       else{
-			       ejecutar_linea_ordenes(orden);
+			       ejecutar_linea_ordenes(orden);	//Ejecuta la orden externa
 		       }
 		else{
 			exit(1);
